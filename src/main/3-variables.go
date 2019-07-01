f1ca1267a8e4f2df440e07ef5a7228f77d0a1c92
yaohua.wu <pandawuu@163.com>
2019-07-01 21:03:28 +0800

    go test project init

---
+++ src/main/3-variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d int
+	fmt.Println(d)
+
+	var e = false
+	fmt.Println(e)
+
+	f := "gogogo"
+	fmt.Println(f)
+}
